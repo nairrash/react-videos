04ffe9a57a3ad625126a304633950a5a45dbdd7b
rashmi <rashmi@Rashmis-MacBook-Pro.local>
2016-10-27 17:57:49 -0700

    arrow functions and webpack update

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,19 +2,17 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
-const MyTitle = require('./MyTitle')
-const myTitleFact = React.createFactory(MyTitle)
 
-var MyFirstComponent = function() {
- return (
-   <div>
-     <MyTitle title="watves" color="mediumaquamarine" />
-     <MyTitle title="LOL" color="papayawhip" />
-     <MyTitle title="gdjwegfjregfk" color="#f06d06" />
+var App = ()=> (
+  <div className="app-container">
+    <div className="home-info">
+      <h1 className="title">svideo</h1>
+      <input className="search" type="text" placeholder="Search"/>
+      <button className="browse-all"> or Browse All! </button>
+    </div>
 
+  </div>
 
-   </div>
-  )
-}
+)
 
-ReactDOM.render(<MyFirstComponent/>, document.getElementById('app'))
+ReactDOM.render(<App/>, document.getElementById('app'))
